df9cc5adf41a0c4d96dac43045bbb84b0f82ded5
Hannes <916200356@qq.com>
2021-12-21 20:49:50 +0800

    Update test.py

--- KNN/test.py
+++ KNN/test.py
@@ -1,32 +1,18 @@
 import numpy as np
-from KNN import *
+import random
+import time
+from KNN import KNN
 
 
-def tst_kd_tree():
-    def preorder_traverse(node):
-        if node is None:
-            print()
-            return
-        print(node.X, node.y)
-        print('left:', end='')
-        preorder_traverse(node.left)
-        print('right:', end='')
-        preorder_traverse(node.right)
-        print('back')
-
-    X = np.array([[1, 4], [7, 2], [8, 1], [2, 3], [4, 7], [9, 6]])
-    y = ['a', 'a', 'b', 'b', 'a', 'b']
-    root = KDTree.construct_kd_tree(X, y)
-    preorder_traverse(root)
-
-
-def tst_knn():
+def tst_knn1():
     X = np.array([[12, 7], [22, 12], [81, 41], [25, 36], [49, 77], [93, 60], [25, 56], [67, 12], [70, 80], [50, 32],
                   [34, 90], [23, 55], [56, 3], [34, 76], [45, 44], [60, 60], [4, 12], [3, 43], [25, 87], [83, 25],
-                  [90, 12], [43, 88], [89, 91], [33, 25], [39, 97], [71, 56], [25, 76], [50, 50], [6, 79], [58, 2]])
+                  [90, 12], [43, 88], [89, 91], [33, 25], [39, 97], [71, 56], [25, 76], [50, 50], [6, 79], [58, 2],
+                  [6, 60], [41, 3], [13, 20], [29, 12], [63, 34], [40, 19], [72, 2], [80, 83], [16, 89], [57, 83]])
     y = np.array([0, 0, 1, 0, 1, 1, 0, 0, 1, 0,
                   1, 0, 0, 0, 0, 0, 0, 0, 1, 1,
-                  1, 1, 1, 0, 1, 1, 1, 0, 1, 0])
+                  1, 1, 1, 0, 1, 1, 1, 0, 1, 0,
+                  1, 0, 0, 0, 0, 0, 1, 1, 1, 1])
     from matplotlib import pyplot as plt
     plt.title('KNN Example')
     plt.xlabel('x1')
@@ -36,7 +22,7 @@ def tst_knn():
 
     neigh = KNN(k_neighbors=5)
     neigh.fit(X, y)
-    X0 = [50, 52]
+    X0 = [38, 60]
 
     plt.plot(X0[0], X0[1], '^g')
     y0 = neigh.predict(X0)
@@ -45,27 +31,44 @@ def tst_knn():
     print(y0)
 
 
+def load_data(file_path, size=0):
+    print('start read file', file_path)
+    with open(file_path) as f:
+        features = []
+        labels = []
+        lines = f.readlines()[:size] if size else f.readlines()
+        random.shuffle(lines)
+        for line in lines:
+            cur_line = line.strip().split(',')
+            features.append([int(num) for num in cur_line[1:]])
+            labels.append(int(cur_line[0]))
+
+        print('end read file, items number:', len(labels), 'feature number:', len(features[0]))
+        return features, labels
+
+
 def tst_knn2():
-    X = np.array([[0, 97, 15], [12, 12, 87], [81, 40, 77], [25, 36, 12], [4, 77, 14],
-                  [93, 60, 88], [25, 59, 32], [67, 12, 25], [70, 80, 14], [55, 32, 12],
-                  [34, 90, 67], [23, 55, 78], [66, 3, 33], [34, 76, 98], [45, 44, 76],
-                  [60, 60, 77], [4, 12, 33], [3, 43, 67], [25, 87, 83], [83, 25, 38],
-                  [90, 12, 27], [43, 88, 32], [89, 91, 77], [33, 25, 87], [39, 97, 38],
-                  [71, 56, 25], [25, 76, 88], [37, 73, 36], [6, 79, 44], [58, 2, 23],
-                  [65, 78, 33], [79, 87, 14], [81, 30, 68], [31, 12, 24], [77, 32, 90],
-                  [32, 40, 9], [9, 40, 23], [88, 70, 2], [67, 89, 72], [41, 13, 88]])
-    y = np.array([2, 1, 5, 0, 2, 7, 2, 4, 6, 4,
-                  3, 3, 4, 3, 1, 7, 0, 1, 3, 4,
-                  4, 2, 1, 7, 2, 4, 3, 2, 2, 4,
-                  6, 6, 5, 0, 5, 0, 0, 6, 7, 1])
-    neigh = KNN(k_neighbors=3)
-    neigh.fit(X, y)
-    X0 = [93, 93, 3]
+    X, y = load_data('../data_set/rand_train.csv', 160000)
+    start_time = time.time()
+    model = KNN(k_neighbors=25)
+    print('fit data...')
+    model.fit(X, y)
+    end_time = time.time()
+    print('Total training time: {}s.'.format(end_time - start_time))
 
-    y0 = neigh.predict(X0)
+    Xt, yt = load_data('../data_set/rand_test.csv', 1000)
+    start_time = time.time()
+    y0 = []
+    print('start prediction...')
+    for i in range(len(Xt)):
+        if i % 200 == 0 and i > 0:
+            print('finish {} predictions, used time {}s'.format(i, time.time() - start_time))
+        y0.append(model.predict(Xt[i]))
+    precision = np.sum([int(yt[i] == y0[i]) for i in range(len(yt))]) / len(yt)
+    print('precision: {}%'.format(precision * 100))
+    end_time = time.time()
+    print('Total testing time: {}'.format(end_time - start_time))
 
-    print(y0)
 
-
-# tst_kd_tree()
-tst_knn()
+tst_knn1()
+# tst_knn2()
